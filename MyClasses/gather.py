3471a8f1b00bb608ec40362869739614d63e6b49
Seonhyun_Kim <sun12334567@gmail.com>
2022-06-13 14:27:06 +0900

    소스를 메인 소스에서 while문이 반복하도록 수정

--- MyClasses/gather.py
+++ MyClasses/gather.py
@@ -1,5 +1,6 @@
 from dht22_to_class import dht22_sensor
 from flamesensor_to_class import flame_sensor
+import time
 
 dht22 = dht22_sensor()
 flame = flame_sensor()
@@ -7,3 +8,4 @@ flame = flame_sensor()
 while(true) :
     dht22.run()
     flame.read_flame_sensor()
+    time.sleep(2)
